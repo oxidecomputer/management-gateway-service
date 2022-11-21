cdbf2bc4d09eb3c8e0d69f25961992a059eaf6a9
John Gallagher <john@oxidecomputer.com>
2022-11-21 17:38:34 -0500

    Add check in TLV parsing for lack of progress

--- gateway-sp-comms/src/single_sp.rs
+++ gateway-sp-comms/src/single_sp.rs
@@ -339,6 +339,15 @@ impl SingleSp {
                     );
                 }
             }
+
+            // Did our number of entries change? If not, we're presumably unable
+            // to parse the response (unknown TLV tags, perhaps) and won't make
+            // forward progress by retrying.
+            if entries.len() as u32 == offset {
+                return Err(SpCommunicationError::TlvPagination {
+                    reason: "failed to parse any entries from SP response",
+                });
+            }
         }
 
         Ok(entries)
