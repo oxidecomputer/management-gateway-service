4deb5929bb06cae9c36259db50e01839537b505c
Ben Stoltz <ben.stoltz@oxidecomputer.com>
2025-03-29 01:16:13 -0700

    Refactoring
    Move RawHubrisArchive knowledge to a separate source file.

--- faux-mgs/src/rhaiscript.rs
+++ faux-mgs/src/rhaiscript.rs
@@ -1,256 +1,29 @@
-//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use crate::anyhow;
-use crate::debug;
-use crate::info;
-use crate::warn;
 use slog::crit;
 use slog::error;
 use slog::trace;
 
-use crate::fs;
-use crate::json;
-use crate::run_command;
-use crate::Context;
-use crate::Logger;
-use crate::Output;
-use crate::Path;
-use crate::PathBuf;
-use crate::Result;
-use crate::RhaiArgs;
-use crate::RotBootInfo;
-use crate::SingleSp;
+use crate::{anyhow, debug, info, warn};
+use crate::{
+    fs, json, run_command, Context, Logger, Output, PathBuf, Result, RhaiArgs,
+    RotBootInfo, SingleSp,
+};
 use clap::Parser;
-use std::sync::Arc;
 
 use async_recursion::async_recursion;
-use hubtools::{Caboose, RawHubrisArchive};
-use lpc55_areas::{CFPAPage, CMPAPage};
 use rhai::packages::Package;
 use rhai::{
-    Array, CustomType, Dynamic, Engine, EvalAltResult, ImmutableString, Map,
-    NativeCallContext, Scope, TypeBuilder,
+    Array, Dynamic, Engine, EvalAltResult, ImmutableString, Map,
+    NativeCallContext, Scope,
 };
 use rhai_chrono::ChronoPackage;
 use rhai_env::EnvironmentPackage;
 use rhai_fs::FilesystemPackage;
-use serde_json::Value as Json;
-use toml::Value as Toml;
 
-#[derive(Debug, CustomType)]
-#[rhai_type(extra = Self::build_archive_inspector)]
-struct ArchiveInspector {
-    #[rhai_type(skip)]
-    inner: Arc<RawHubrisArchive>,
-}
-
-impl Clone for ArchiveInspector {
-    fn clone(&self) -> Self {
-        ArchiveInspector { inner: self.inner.clone() }
-    }
-}
-
-impl ArchiveInspector {
-    fn new(inner: Arc<RawHubrisArchive>) -> Self {
-        ArchiveInspector { inner }
-    }
-
-    pub fn from_vec(contents: Vec<u8>) -> Result<Self, Box<EvalAltResult>> {
-        match RawHubrisArchive::from_vec(contents) {
-            Ok(archive) => Ok(Self::new(Arc::new(archive))),
-            Err(e) => Err(format!("RawHubrisArchive::from_vec: {e}")
-                .to_string()
-                .into()),
-        }
-    }
-
-    pub fn load(path: ImmutableString) -> Result<Self, Box<EvalAltResult>> {
-        let path = PathBuf::from(path.into_owned());
-        match RawHubrisArchive::load(&path) {
-            Ok(archive) => Ok(Self::new(Arc::new(archive))),
-            Err(e) => {
-                Err(format!("RawHubrisArchive::load: {e}").to_string().into())
-            }
-        }
-    }
-
-    fn u8_to_string(array: &[u8]) -> String {
-        String::from_utf8_lossy(
-            if let Some(p) = array.iter().position(|&x| x == 0) {
-                &array[0..p]
-            } else {
-                &array[0..]
-            },
-        )
-        .to_string()
-    }
-
-    pub fn indexer(
-        &mut self,
-        index: &str,
-    ) -> Result<Dynamic, Box<EvalAltResult>> {
-        match index {
-            // Copied from hubtools/src/caboose
-            "BORD" => {
-                let caboose: Caboose = self
-                    .inner
-                    .read_caboose()
-                    .map_err(|e| format!("RawArchive::read_caboose: {e:?}"))?;
-                caboose
-                    .board()
-                    .map(|v| Ok(Self::u8_to_string(v).into()))
-                    .unwrap_or(Ok(Dynamic::UNIT))
-            }
-            "GITC" => {
-                let caboose: Caboose = self
-                    .inner
-                    .read_caboose()
-                    .map_err(|e| format!("RawArchive::read_caboose: {e:?}"))?;
-                caboose
-                    .git_commit()
-                    .map(|v| Ok(Self::u8_to_string(v).into()))
-                    .unwrap_or(Ok(Dynamic::UNIT))
-            }
-            "NAME" => {
-                let caboose: Caboose = self
-                    .inner
-                    .read_caboose()
-                    .map_err(|e| format!("RawArchive::read_caboose: {e:?}"))?;
-                caboose
-                    .name()
-                    .map(|v| Ok(Self::u8_to_string(v).into()))
-                    .unwrap_or(Ok(Dynamic::UNIT))
-            }
-            "SIGN" => {
-                let caboose: Caboose = self
-                    .inner
-                    .read_caboose()
-                    .map_err(|e| format!("RawArchive::read_caboose: {e:?}"))?;
-                caboose
-                    .sign()
-                    .map(|v| Ok(Self::u8_to_string(v).into()))
-                    .unwrap_or(Ok(Dynamic::UNIT))
-            }
-            "VERS" => {
-                let caboose: Caboose = self
-                    .inner
-                    .read_caboose()
-                    .map_err(|e| format!("RawArchive::read_caboose: {e:?}"))?;
-                caboose
-                    .version()
-                    .map(|v| Ok(Self::u8_to_string(v).into()))
-                    .unwrap_or(Ok(Dynamic::UNIT))
-            }
-            "image_name" => {
-                if let Ok(iname) = self.inner.image_name() {
-                    Ok(Dynamic::from(iname))
-                } else {
-                    Ok(Dynamic::UNIT)
-                }
-            }
-            // Only in Bootleby versions v1.3.1 and later
-            // "manufacturing_cfg" => {..}
-            _ => {
-                // Try to extract a file by this name
-                if let Ok(contents) = self.inner.extract_file(index) {
-                    // contents: Vec<u8>
-                    match Path::new(index)
-                        .extension()
-                        .and_then(|os| os.to_str())
-                    {
-                        Some("bin") | Some("elf") => {
-                            Ok(Dynamic::from_blob(contents))
-                        }
-                        Some("toml") => {
-                            // Adapted from toml crate example toml2json
-                            fn toml2json(tv: Toml) -> Json {
-                                match tv {
-                                    Toml::String(s) => Json::String(s),
-                                    Toml::Integer(i) => Json::Number(i.into()),
-                                    Toml::Float(f) => {
-                                        if let Some(n) =
-                                            serde_json::Number::from_f64(f)
-                                        {
-                                            Json::Number(n)
-                                        } else {
-                                            Json::Null
-                                        }
-                                    }
-                                    Toml::Boolean(b) => Json::Bool(b),
-                                    Toml::Array(arr) => Json::Array(
-                                        arr.into_iter()
-                                            .map(toml2json)
-                                            .collect(),
-                                    ),
-                                    Toml::Table(table) => Json::Object(
-                                        table
-                                            .into_iter()
-                                            .map(|(k, v)| (k, toml2json(v)))
-                                            .collect(),
-                                    ),
-                                    Toml::Datetime(dt) => {
-                                        Json::String(dt.to_string())
-                                    }
-                                }
-                            }
-                            let text = String::from_utf8_lossy(&contents[..])
-                                .to_string();
-                            if let Json::Object(json) =
-                                toml2json(Toml::from(text))
-                            {
-                                Ok(Dynamic::from(json))
-                            } else {
-                                unreachable!();
-                            }
-                        }
-                        Some("json") => {
-                            let text: String =
-                                String::from_utf8_lossy(&contents[..])
-                                    .to_string();
-                            if let Ok(json) = serde_json::to_value(text) {
-                                Ok(Dynamic::from(json))
-                            } else {
-                                let text =
-                                    String::from_utf8_lossy(&contents[..])
-                                        .to_string();
-                                if let Json::Object(json) = json!({ "Err": serde_json::Value::String(text) })
-                                {
-                                    Ok(Dynamic::from(json))
-                                } else {
-                                    unreachable!()
-                                }
-                            }
-                        }
-                        _ => {
-                            if index.starts_with("elf/") {
-                                // All of these are binary files
-                                Ok(Dynamic::from_blob(contents))
-                            } else {
-                                // .txt, .fwid, .ron, .tlvc, .cfg, .gdb
-                                // git-rev, .TXT, image-name
-                                let text =
-                                    String::from_utf8_lossy(&contents[..])
-                                        .to_string();
-                                Ok(Dynamic::from(text))
-                            }
-                        }
-                    }
-                } else {
-                    Err(format!("unknown index: {:?}", index).into())
-                }
-            }
-        }
-    }
-
-    pub fn build_archive_inspector(builder: &mut TypeBuilder<Self>) {
-        builder
-            .with_name("archive")
-            .with_fn("new_archive", ArchiveInspector::from_vec)
-            .with_fn("new_archive", ArchiveInspector::load)
-            .with_indexer_get(ArchiveInspector::indexer);
-    }
-}
+mod hubris;
 
 /// Use a Rhai interpreter per SingleSp that can maintain a connection.
 #[async_recursion]
@@ -289,14 +62,22 @@ pub async fn interpreter(
         // Don't limit resources for now.
         engine.set_max_expr_depths(0, 0);
 
-        // Access RawHubrisArchives
-        engine.build_type::<ArchiveInspector>();
+        // Access RawHubrisArchives and their Cabooses
+        engine.build_type::<hubris::ArchiveInspector>();
+        engine.build_type::<hubris::CabooseInspector>();
         engine.register_fn("system", system);
 
         // Compile the script
-        let program = fs::read_to_string(&script)
-            .with_context(|| format!("failed to read {}", script.display()))
-            .unwrap();
+        let program = match fs::read_to_string(&script) {
+            Ok(content) => content,
+            Err(e) => {
+                return Err(anyhow!(
+                    "failed to read {}: {}",
+                    script.display(),
+                    e
+                ));
+            }
+        };
 
         // Construct argv for the script and canonicalize the script path.
         let pb = fs::canonicalize(&script)
@@ -310,7 +91,7 @@ pub async fn interpreter(
 
         engine
             // faux_mgs thread consumes and produces JSON
-            .register_fn("faux_mgs", move |v: Dynamic| -> Dynamic {
+            .register_fn("faux_mgs", move |v: Array| -> Dynamic {
                 match tx_script.send(serde_json::to_string(&v).unwrap()) {
                     Ok(()) => match rx_script.recv() {
                         Ok(v) => {
@@ -348,39 +129,7 @@ pub async fn interpreter(
                         serde_json::from_str::<Dynamic>(&err).unwrap()
                     }
                 }
-            })
-            // lpc55_support RoT signature verification
-            .register_fn(
-                "verify_rot_image",
-                move |image: Dynamic,
-                      cmpa: Dynamic,
-                      cfpa: Dynamic|
-                      -> Dynamic {
-                    fn to_page(slice: &[u8]) -> Option<&[u8; 512]> {
-                        slice.try_into().ok()
-                    }
-
-                    let a = cmpa.as_blob_ref().unwrap().clone();
-                    let page = to_page(a.as_ref()).unwrap();
-                    let cmpapage = CMPAPage::from_bytes(page).unwrap();
-
-                    let a = cfpa.as_blob_ref().unwrap().clone();
-                    let page = to_page(a.as_ref()).unwrap();
-                    let cfpapage = CFPAPage::from_bytes(page).unwrap();
-
-                    let image = image.as_blob_ref().unwrap().clone();
-
-                    if lpc55_sign::verify::verify_image(
-                        &image, cmpapage, cfpapage,
-                    )
-                    .is_ok()
-                    {
-                        true.into()
-                    } else {
-                        false.into()
-                    }
-                },
-            );
+            });
 
         // A script can log via debug at any level:
         //   debug("INFO|log message at INFO level");
@@ -427,7 +176,6 @@ pub async fn interpreter(
         });
 
         // Print registered functions if you're interested.
-
         // engine.gen_fn_signatures(false).into_iter().for_each(|func| println!("{func}"));
 
         match engine.compile(program) {
@@ -464,21 +212,28 @@ pub async fn interpreter(
     });
 
     while let Ok(command_args) = rx_master.recv() {
-        // Service the script's calls to "faux_mgs"
-        // from those commands.
-        // The script can only send use arrays of strings.
-        // println!("args=${:?}", &command_args);
-        let response = if let Ok(serde_json::Value::Array(j)) =
+        // Service the script's calls to "faux_mgs".
+        // The script can only send arrays of string and i64 values.
+        let response = if let Ok(serde_json::Value::Array(script_args)) =
             serde_json::from_str(&command_args)
         {
-            // TODO: fix this so that user can put an i64 and we won't panic.
-            let a: Vec<String> =
-                j.iter().map(|v| v.as_str().unwrap().to_string()).collect();
-            debug!(log, "vec string: {:?}", a);
+            // TODO: Check for non-string non-i64 values in the
+            // script_args and return an error instead of executing the faux-mgs
+            // command.
+            let faux_mgs_args: Vec<String> = script_args
+                .iter()
+                .map(|v| {
+                    v.as_str()
+                        .map(|s| s.to_string())
+                        .or_else(|| v.as_i64().map(|i| i.to_string()))
+                        .unwrap()
+                })
+                .collect();
+            debug!(log, "vec string: {:?}", faux_mgs_args);
             let mut ra = vec![];
             // The clap crate is expecting ARGV[0] as the program name, insert a dummy.
             ra.push("faux-mgs".to_string());
-            ra.append(&mut a.clone());
+            ra.append(&mut faux_mgs_args.clone());
 
             let args = RhaiArgs::parse_from(&ra);
             match run_command(sp, args.command.clone(), true, log.clone()).await
@@ -525,43 +280,45 @@ pub async fn interpreter(
     }
 }
 
-/// Allow Rhai scripts to run commands and capture stdout, stderr, and
+//
+// This function was generated with the following prompt to
+// gemini.google.com:
+//
+// Write a Rust function, `system`, that can be registered with the Rhai
+// scripting engine. The function should take an array of strings (`Array`)
+// as input, representing a command and its arguments, execute the command
+// using `std::process::Command`, and return a Rhai `Map` containing the
+// command's exit code, standard output, and standard error.
+//
+// The function should handle the following:
+//
+// * Convert the input `Array` to a `Vec<String>`.
+// * Handle errors if the input `Array` is empty or if any element cannot
+//   be converted to a `String`.
+// * Use `std::process::Command` with fully qualified names (e.g.,
+//   `std::process::Command::new`).
+// * Capture the command's standard output and standard error using
+//   `std::process::Stdio::piped()`.
+// * Convert the captured output to Rhai `ImmutableString` values using
+//   `String::from_utf8_lossy`.
+// * Return a Rhai `Map` with the keys "exit_code", "stdout", and "stderr".
+// * Handle errors during command execution and output capture.
+// * Use `EvalAltResult::ErrorInFunctionCall` for function call errors and
+//   `EvalAltResult::ErrorRuntime` for runtime errors.
+// * Ensure that error messages passed to `EvalAltResult::ErrorRuntime`
+//   are converted to `Dynamic` using `.into()`.
+// * Place the underlying error in the third position of the
+//   `EvalAltResult::ErrorInFunctionCall` variant.
+// * Use `context.position()` to get the error position.
+// * Do not use the `mut` keyword on the `child` variable when calling
+//   `command.spawn()`.
+//
+// Provide a complete Rust code example that includes the `system` function
+// and a `main` function that registers it with a Rhai engine and runs a
+// sample Rhai script.
+
+/// Allow Rhai scripts to run a command and capture the stdout, stderr, and
 /// exit code.
-/// This function was generated with the following prompt to
-/// gemini.google.com:
-///
-/// Write a Rust function, `system`, that can be registered with the Rhai
-/// scripting engine. The function should take an array of strings (`Array`)
-/// as input, representing a command and its arguments, execute the command
-/// using `std::process::Command`, and return a Rhai `Map` containing the
-/// command's exit code, standard output, and standard error.
-///
-/// The function should handle the following:
-///
-/// * Convert the input `Array` to a `Vec<String>`.
-/// * Handle errors if the input `Array` is empty or if any element cannot
-///   be converted to a `String`.
-/// * Use `std::process::Command` with fully qualified names (e.g.,
-///   `std::process::Command::new`).
-/// * Capture the command's standard output and standard error using
-///   `std::process::Stdio::piped()`.
-/// * Convert the captured output to Rhai `ImmutableString` values using
-///   `String::from_utf8_lossy`.
-/// * Return a Rhai `Map` with the keys "exit_code", "stdout", and "stderr".
-/// * Handle errors during command execution and output capture.
-/// * Use `EvalAltResult::ErrorInFunctionCall` for function call errors and
-///   `EvalAltResult::ErrorRuntime` for runtime errors.
-/// * Ensure that error messages passed to `EvalAltResult::ErrorRuntime`
-///   are converted to `Dynamic` using `.into()`.
-/// * Place the underlying error in the third position of the
-///   `EvalAltResult::ErrorInFunctionCall` variant.
-/// * Use `context.position()` to get the error position.
-/// * Do not use the `mut` keyword on the `child` variable when calling
-///   `command.spawn()`.
-///
-/// Provide a complete Rust code example that includes the `system` function
-/// and a `main` function that registers it with a Rhai engine and runs a
-/// sample Rhai script.
 fn system(
     context: NativeCallContext,
     argv: Array,
