f227b598d61b86f802ead2092076576059f36373
Alex Plotnick <alex@oxidecomputer.com>
2025-11-14 09:38:20 -0700

    faux-mgs: de-conflict permslip --ssh-auth-sock (#470)
    Unlocking with permslip can (and will in production) use the
    SSH_AUTH_SOCK to authenticate to the server (but not to sign
    the challenge response).

--- faux-mgs/src/main.rs
+++ faux-mgs/src/main.rs
@@ -571,7 +571,6 @@ enum MonorailCommand {
             long,
             alias = "online",
             conflicts_with = "list",
-            conflicts_with = "ssh_auth_sock",
             requires = "key"
         )]
         permslip: bool,
